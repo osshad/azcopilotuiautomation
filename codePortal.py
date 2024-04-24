32f4e9d462db6e23be40bf18f012b8a1c8f7e508
osshad <oluwaseun4life@gmail.com>
2024-04-24 17:18:34 -0500

    Update codePortal.py

--- codePortal.py
+++ codePortal.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import os, re, time, sys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-
-# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
